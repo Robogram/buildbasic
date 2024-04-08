7013a075fe4736c006204aeef535324b8301ebda
Kevin Mai <kevinmai@Kevins-MacBook-Pro.local>
2024-04-08 19:11:50 -0400

    initialized react app and a few headers

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div id="app">
+      <div className="header">Let's build something simple and fun:)</div>
+
+      <div className="header">Let's build a simple calculator</div>
+
+      <div id="phone">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
