3e75331c8ca81bd45a15f991ea1126c8496c49dc
Kevin Mai <kevinmai@Kevins-MacBook-Pro.local>
2024-04-09 10:11:03 -0400

    designed the setup on phone and macbook

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ function App() {
         </div>
       </div>
 
-      <div className="header" style={{ marginTop: 100 }}>Now, Let's quickly build your own</div>
+      <div className="header" style={{ marginTop: 300 }}>Now, Let's quickly build your own</div>
       <div className="header">Get the only app you need for this</div>
 
       <div id="needs">
@@ -93,7 +93,62 @@ function App() {
         <img style={{ height: '100%', width: '100%' }} src="/expo-newuser.jpeg"/>
       </div>
 
-      <div className="header">Done, now let's get to the real stuff on your macbook</div>
+      <div className="header">DONE</div>
+
+      <div className="header" style={{ marginTop: 200 }}>Go on your Macbook</div>
+
+      <div className="headers">
+        <div className="header-item">
+          <div className="header-item-header">1. Get homebrew on your macbook</div>
+          <ul>
+            <li>Homebrew is a software package that lets you install the softwares you need on your macbook to build apps</li>
+            <li>On your macbook, Open terminal inside "/Applications/Utilities/Terminal"</li>
+            <li>
+              You should see something like this
+              <img style={{ height: 500, width: 500 }} src="/terminal-shot.jpeg"/>
+            </li>
+            <li>Copy and paste the red text and hit "ENTER" to install Homebrew,
+              <br/><br/>
+              <strong>/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"</strong>
+              <br/><br/>
+            </li>
+            <li>It will take about a minute or 2 so just wait</li>
+            <li>If it asks something like "[Y/n]", just hit "ENTER"</li>
+            <li>DONE! Now you need to install the software package "yarn" and use that to create mobile app projects</li>
+            <li>
+              Type this into your terminal and hit "ENTER" to install "Yarn"
+              <br/><br/>
+              <strong>brew install yarn</strong>
+              <br/><br/>
+            </li>
+            <li>DONE:)</li>
+            <li>
+              Inside your terminal, navigate to your desktop by typing
+              <br/><br/>
+              <strong>~/desktop</strong>
+              <br/><br/>
+            </li>
+            <li>
+              Create a mobile app project
+              <br/><br/>
+              <strong>npx create-expo-app the-calculator</strong>
+              <br/><br/>
+            </li>
+            <li>Once done, you should see "✅ Your project is ready!" on your terminal</li>
+            <li>
+              Now, login into your expo account on your terminal
+              <br/><br/>
+              <strong>npx expo login</strong>
+              <br/><br/>
+            </li>
+            <li>Username/Email Address: [Enter your username or email]</li>
+            <li>Password: [Enter your password]</li>
+            <li>Once done, you should see "Success. You are now logged in as [your username]"</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="header">YOUR ARE DONE WITH THE SETUP. LET'S BEGIN BUILDING</div>
     </div>
   );
 }
